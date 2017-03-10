7bb740f532cc38994843d9dabd8ffb4285176dda
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-10 19:16:42 +0100

    Initial commit.

---
+++ src/Storage_WordPress_Option/Storage_WordPress_Option.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Storage_WordPress_Option class
+ *
+ * @package APIAPIStorageWordPressOption
+ * @since 1.0.0
+ */
+
+namespace APIAPI\Storage_WordPress_Option;
+
+use APIAPI\Core\Storages\Array_Storage;
+
+if ( ! class_exists( 'APIAPI\Storage_WordPress_Option\Storage_WordPress_Option' ) ) {
+
+	/**
+	 * Storage implementation using WordPress options.
+	 *
+	 * @since 1.0.0
+	 */
+	class Storage_WordPress_Option extends Array_Storage {
+		/**
+		 * Gets the array values are stored in.
+		 *
+		 * @since 1.0.0
+		 * @access protected
+		 *
+		 * @param string $basename The basename under which to store.
+		 * @return array Array with stored data.
+		 */
+		protected function get_array( $basename ) {
+			return get_option( $basename, array() );
+		}
+
+		/**
+		 * Updates the array values are stored in.
+		 *
+		 * @since 1.0.0
+		 * @access protected
+		 *
+		 * @param string $basename The basename under which to store.
+		 * @param array  $data     Array with updated data.
+		 */
+		protected function update_array( $basename, $data ) {
+			update_option( $basename, $data );
+		}
+
+		/**
+		 * Deletes the array values are stored in.
+		 *
+		 * @since 1.0.0
+		 * @access protected
+		 *
+		 * @param string $basename The basename under which to store.
+		 */
+		protected function delete_array( $basename ) {
+			delete_option( $basename );
+		}
+	}
+
+}
